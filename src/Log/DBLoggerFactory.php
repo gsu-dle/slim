f3237dfb4d74306013c4d4d101fbb6cc2bb2ac3f
Melody Forest <mforest@gsu.edu>
2023-03-06 15:11:45 -0500

    Added `logTable` as a setting param in constructor

--- src/Log/DBLoggerFactory.php
+++ src/Log/DBLoggerFactory.php
@@ -7,19 +7,19 @@ namespace GAState\Web\Slim\Log;
 use GAState\Web\Slim\Log\LoggerFactoryInterface as LoggerFactory;
 use RuntimeException                            as RuntimeException;
 use Monolog\Formatter\LineFormatter             as MonologLineFormatter;
-use Monolog\Handler\StreamHandler               as MonologStreamHandler;
 use Monolog\Level                               as MonologLevel;
 use Monolog\Logger                              as MonologLogger;
 use Monolog\Processor\ProcessIdProcessor        as MonologProcessIdProcessor;
 use Monolog\Processor\UidProcessor              as MonologUidProcessor;
 use Monolog\Processor\WebProcessor              as MonologWebProcessor;
-use PDO;
+use PDO                                         as PDO;
 use Psr\Http\Message\ServerRequestInterface     as Request;
 use Psr\Log\LoggerInterface                     as Logger;
 
 class DBLoggerFactory implements LoggerFactory
 {
     private string $logName;
+    private string $logTable;
     private string $logLevel;
     private PDO $pdo;
     private Request $request;
@@ -27,17 +27,20 @@ class DBLoggerFactory implements LoggerFactory
 
     /**
      * @param string $logName
+     * @param string $logTable
      * @param string $logLevel
      * @param PDO $pdo
      * @param Request $request
      */
     public function __construct(
         string $logName,
+        string $logTable,
         string $logLevel,
         PDO $pdo,
         Request $request
     ) {
         $this->logName = $logName;
+        $this->logTable = $logTable;
         $this->logLevel = $logLevel;
         $this->pdo = $pdo;
         $this->request = $request;
@@ -56,7 +59,7 @@ class DBLoggerFactory implements LoggerFactory
         $logger = new MonologLogger($this->logName);
         $handler = new DBLogHandler(
             $this->pdo,
-            'WebLog',
+            $this->logTable,
             [
                 'uid',
                 'process_id',
